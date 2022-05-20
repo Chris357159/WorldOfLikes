4d9e83f48466613928202079c326ed5a4d44c9b0
Chris357159 <cs.activconsult@outlook.fr>
2022-05-20 14:41:09 +0200

    Creation table restaurant

--- src/main/java/com/projetspring/WorldsOfLikes/beans/Restauration.java
+++ src/main/java/com/projetspring/WorldsOfLikes/beans/Restauration.java
@@ -8,44 +8,22 @@ import javax.persistence.Id;
 
 @Entity
 public class Restauration {
+    //Attribute
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idResto;
     private String nomResto;
     private String adresseResto;
 
+    //Consctructors
     public Restauration() {
     }
-
     public Restauration(String nomResto, String adresseResto) {
         this.nomResto = nomResto;
         this.adresseResto = adresseResto;
     }
 
-    public int getIdResto() {
-        return idResto;
-    }
-
-    public void setIdResto(int idResto) {
-        this.idResto = idResto;
-    }
-
-    public String getNomResto() {
-        return nomResto;
-    }
-
-    public void setNomResto(String nomResto) {
-        this.nomResto = nomResto;
-    }
-
-    public String getAdresseResto() {
-        return adresseResto;
-    }
-
-    public void setAdresseResto(String adresseResto) {
-        this.adresseResto = adresseResto;
-    }
-
+    //Methods
     @Override
     public String toString() {
         return "Restauration{" +
@@ -53,5 +31,25 @@ public class Restauration {
                 ", nomResto='" + nomResto + '\'' +
                 ", adresseResto='" + adresseResto + '\'' +
                 '}';
+    }
+
+    //Get/Set
+    public int getIdResto() {
+        return idResto;
+    }
+    public void setIdResto(int idResto) {
+        this.idResto = idResto;
+    }
+    public String getNomResto() {
+        return nomResto;
+    }
+    public void setNomResto(String nomResto) {
+        this.nomResto = nomResto;
+    }
+    public String getAdresseResto() {
+        return adresseResto;
+    }
+    public void setAdresseResto(String adresseResto) {
+        this.adresseResto = adresseResto;
     }
 }
--- src/main/java/com/projetspring/WorldsOfLikes/controllers/Controller_Delevery.java
+++ src/main/java/com/projetspring/WorldsOfLikes/controllers/Controller_Delevery.java
@@ -2,6 +2,8 @@ package com.projetspring.WorldsOfLikes.controllers;
 
 
 import com.projetspring.WorldsOfLikes.beans.Restauration;
+import com.projetspring.WorldsOfLikes.repositories.RestoRepositoryInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/Delevery")
 
-public class Conroller_Delevery {
+public class Controller_Delevery {
+
+    //Create DB
+    @Autowired
+    private RestoRepositoryInterface restoRepositoryInterface;
+
     @GetMapping("/ajoutResto")
     public String ajoutResto(){
         Restauration resto1=new Restauration();
