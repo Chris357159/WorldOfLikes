a8902210e18a37a163d3b6bf2fe2ddaed689c305
Chris357159 <cs.activconsult@outlook.fr>
2022-05-20 10:53:38 +0200

    Init projet

---
+++ src/main/java/com/projetspring/WorldsOfLikes/WorldsOfLikesApplication.java
@@ -0,0 +1,13 @@
+package com.projetspring.WorldsOfLikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldsOfLikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldsOfLikesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetspring/WorldsOfLikes/WorldsOfLikesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetspring.WorldsOfLikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldsOfLikesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
