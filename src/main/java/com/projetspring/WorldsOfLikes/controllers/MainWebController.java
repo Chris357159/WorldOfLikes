0c9a1fd36ecf250a1d3b34d6f14bbc121a0aa4e3
AlexCFs <AlexCFs>
2022-05-23 16:27:11 +0200

    commit projet jeu

--- src/main/java/com/projetspring/WorldsOfLikes/controllers/MainWebController.java
+++ src/main/java/com/projetspring/WorldsOfLikes/controllers/MainWebController.java
@@ -38,7 +38,7 @@ public class MainWebController {
             return "Bad Password";
         }
         else{
-            return ("");
+            return ("" + connexion.getID());
         }
     }
 }
